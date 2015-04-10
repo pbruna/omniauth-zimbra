83a07c0268588e09c739b5e06f8e62ceb76c00bb
Patricio Bruna <pbruna@gmail.com>
2015-04-10 18:13:27 -0300

    Fixed Amavisd pattern

--- lib/omniauth-zimbraadmin/version.rb
+++ lib/omniauth-zimbraadmin/version.rb
@@ -1,5 +1,5 @@
 module Omniauth
   module ZimbraAdmin
-    VERSION = "0.0.3.20"
+    VERSION = "0.0.3.21"
   end
 end
--- lib/omniauth/strategies/zimbra_admin.rb
+++ lib/omniauth/strategies/zimbra_admin.rb
@@ -33,7 +33,7 @@ module OmniAuth
       end
       
       credentials do 
-        { @authentication_response[:token] => authentication_response } 
+        {  token => @authentication_response[:token] } 
       end
       
       extra do 
